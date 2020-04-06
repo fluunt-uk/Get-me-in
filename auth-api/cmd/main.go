edf9fe76d838430b2a4dbf1332e2db7bf5be7383
SweetTreats <sumite3117@hotmail.com>
2020-04-06 22:32:16 +0100

    rollback experiment

--- auth-api/cmd/main.go
+++ auth-api/cmd/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/ProjectReferral/Get-me-in/auth-api/configs"
-	"github.com/ProjectReferral/Get-me-in/auth-api/service"
+	"github.com/ProjectReferral/Get-me-in/auth-api/internal"
 	"log"
 )
 
 func main() {
 	log.Println("Running on %s", configs.PORT)
-	service.SetupEndpoints()
+	internal.SetupEndpoints()
 }
