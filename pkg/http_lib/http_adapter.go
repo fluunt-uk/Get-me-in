d098011ffc48f0b72b8b2d89c2857cf93e4067c5
SweetTreats <sumite3117@hotmail.com>
2020-04-07 21:45:24 +0100

    draft bug fix

--- pkg/http_lib/http_adapter.go
+++ pkg/http_lib/http_adapter.go
@@ -45,11 +45,11 @@ func HttpRequest(url string, body []byte, headers map[string]string, method stri
 		return nil, err
 	}
 
-	if resp.StatusCode != 200 {
-		return nil, &ErrorString{
-			Reason: resp.Status,
-			Code:   resp.StatusCode,
-		}
-	}
+	//if resp.StatusCode != 200 {
+	//	return nil, &ErrorString{
+	//		Reason: resp.Status,
+	//		Code:   resp.StatusCode,
+	//	}
+	//}
 	return resp, nil
 }
