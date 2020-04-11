f317c5fc986cf59d796754a07458f684cd05d220
Hamza <hamza_razeq@hotmail.co.uk>
2020-04-11 17:23:20 +0100

    payment method added

--- payment-api/internal/routes.go
+++ payment-api/internal/routes.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"github.com/ProjectReferral/Get-me-in/payment-api/configs"
 	"github.com/ProjectReferral/Get-me-in/payment-api/lib/stripe-api/resources/customer"
+	"github.com/ProjectReferral/Get-me-in/payment-api/lib/stripe-api/resources/payment"
 	"github.com/ProjectReferral/Get-me-in/payment-api/lib/stripe-api/resources/subscription"
 	"github.com/gorilla/mux"
 	"log"
@@ -24,6 +25,8 @@ func SetupEndpoints() {
 	_router.HandleFunc("/sub/update", subscription.UpdateSub).Methods("POST")
 	_router.HandleFunc("/sub/cancel", subscription.CancelSub).Methods("DELETE")
 	_router.HandleFunc("/sub/getAll", subscription.ListSubs).Methods("GET")
+
+	_router.HandleFunc("/payment/create", payment.CreatePayment).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(configs.PORT, _router))
 }
---
+++ payment-api/lib/stripe-api/resources/payment/response_helper.go
@@ -0,0 +1,17 @@
+package payment
+
+import (
+	"encoding/json"
+	"github.com/ProjectReferral/Get-me-in/payment-api/lib/stripe-api"
+	"github.com/stripe/stripe-go"
+	"net/http"
+)
+
+func ReturnSuccessJSON(w http.ResponseWriter, c *stripe.PaymentMethod) {
+	toString, err := json.Marshal(c)
+
+	if !stripe_api.HandleError(err, w) {
+		w.Write(toString)
+		w.WriteHeader(http.StatusOK)
+	}
+}
