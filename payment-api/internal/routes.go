77a26bab0a3ec008cf592c75344649f479eac75b
SweetTreats <sumite3117@hotmail.com>
2020-05-29 19:13:22 +0100

    add logging to file

--- payment-api/internal/routes.go
+++ payment-api/internal/routes.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ProjectReferral/Get-me-in/payment-api/internal/service"
 	"github.com/ProjectReferral/Get-me-in/pkg/security"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -26,5 +27,13 @@ func (eb *EndpointBuilder) InjectSubscriptionServ(ss service.Subscription) {
 func (eb *EndpointBuilder) SetupEndpoints() {
 
 	eb.router.HandleFunc("/premium/subscribe", security.WrapHandlerWithSpecialAuth(eb.ss.SubscribeToPremiumPlan, configs.AUTH_AUTHENTICATED)).Methods("POST")
+	eb.router.HandleFunc("/log", displayLog).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(configs.PORT, eb.router))
+}
+
+func displayLog(w http.ResponseWriter, r *http.Request){
+	b, _ := ioutil.ReadFile("logs/paymentAPI_log.txt")
+
+	w.Write(b)
 }
